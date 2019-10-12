038b101d92ba4584dd1544cad65d94b3c06c15fd
akshit <akshitgupta925@gmail.com>
2019-10-12 17:58:15 -0400

    added recycler view for list

---
+++ app/src/main/java/com/aki/stepstracker/adapter/StepCountAdapter.java
@@ -0,0 +1,55 @@
+package com.aki.stepstracker.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.aki.stepstracker.R;
+import com.google.android.gms.fitness.data.DataSet;
+
+import java.util.List;
+
+public class StepCountAdapter extends RecyclerView.Adapter<StepCountAdapter.ViewHolder> {
+
+    private List<DataSet> mData;
+    private LayoutInflater mInflater;
+//    private ItemClickListener mClickListener;
+
+    // data is passed into the constructor
+    public StepCountAdapter(Context context, List<DataSet> data) {
+        this.mInflater = LayoutInflater.from(context);
+        this.mData = data;
+    }
+
+    @NonNull
+    @Override
+    public StepCountAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = mInflater.inflate(R.layout.steps_row_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull StepCountAdapter.ViewHolder holder, int position) {
+        String steps = " ";
+        holder.stepsCount.setText(steps);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+        TextView stepsCount;
+
+        ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            stepsCount = itemView.findViewById(R.id.stepsCount);
+        }
+    }
+}
