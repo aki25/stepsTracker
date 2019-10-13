617dfe5eaec1e4327e7d4dd021d7df70390a47f9
akshit <akshitgupta925@gmail.com>
2019-10-13 18:59:13 -0400

    dates need to be midnight to get correct buckets

--- app/src/main/java/com/aki/stepstracker/activity/UserDataActivity.java
+++ app/src/main/java/com/aki/stepstracker/activity/UserDataActivity.java
@@ -6,7 +6,6 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
-import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.GradientDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
@@ -20,11 +19,7 @@ import com.aki.stepstracker.utils.StepsDataParser;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.fitness.Fitness;
 import com.google.android.gms.fitness.data.Bucket;
-import com.google.android.gms.fitness.data.DataPoint;
-import com.google.android.gms.fitness.data.DataSet;
-import com.google.android.gms.fitness.data.DataSource;
 import com.google.android.gms.fitness.data.DataType;
-import com.google.android.gms.fitness.data.Field;
 import com.google.android.gms.fitness.request.DataReadRequest;
 import com.google.android.gms.fitness.result.DataReadResponse;
 import com.google.android.gms.tasks.Task;
@@ -35,11 +30,10 @@ import java.text.DateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
-
-import static java.text.DateFormat.getDateTimeInstance;
 
 public class UserDataActivity extends AppCompatActivity {
 
@@ -90,42 +84,39 @@ public class UserDataActivity extends AppCompatActivity {
     private class ReadData extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... voids) {
-            // Setting a start and end date using a range of 1 week before this moment.
-            Calendar cal = Calendar.getInstance();
-            Date now = new Date();
-            cal.setTime(now);
-            long endTime = cal.getTimeInMillis();
-            cal.add(Calendar.WEEK_OF_YEAR, -2);
-            long startTime = cal.getTimeInMillis();
+            // Setting a start and end date using a range of 2 week before this moment.
 
-            java.text.DateFormat dateFormat = DateFormat.getDateInstance();
+            Calendar calEnd = new GregorianCalendar();
+            calEnd.setTime(new Date());
+            calEnd.set(Calendar.DAY_OF_YEAR, calEnd.get(Calendar.DAY_OF_YEAR));
+            calEnd.set(Calendar.HOUR_OF_DAY, 0);
+            calEnd.set(Calendar.MINUTE, 0);
+            calEnd.set(Calendar.SECOND, 0);
+            calEnd.set(Calendar.MILLISECOND, 0);
+            long endTime = calEnd.getTimeInMillis();
+            calEnd.set(Calendar.DAY_OF_YEAR, calEnd.get(Calendar.DAY_OF_YEAR)-14);
+            long startTime = calEnd.getTimeInMillis();
+
+
+            java.text.DateFormat dateFormat = DateFormat.getDateTimeInstance();
             Log.i(TAG, "Range Start: " + dateFormat.format(startTime));
             Log.i(TAG, "Range End: " + dateFormat.format(endTime));
 
-            DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
-                    .setAppPackageName("com.google.android.gms")
-                    .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
-                    .setType(DataSource.TYPE_DERIVED)
-                    .setStreamName("estimated_steps")
-                    .build();
+//            DataSource ESTIMATED_STEP_DELTAS = new DataSource.Builder()
+//                    .setAppPackageName("com.google.android.gms")
+//                    .setDataType(DataType.TYPE_STEP_COUNT_DELTA)
+//                    .setType(DataSource.TYPE_DERIVED)
+//                    .setStreamName("estimated_steps")
+//                    .build();
 
             DataReadRequest readRequest =
                     new DataReadRequest.Builder()
-                            // The data request can specify multiple data types to return, effectively
-                            // combining multiple data queries into one call.
-                            // In this example, it's very unlikely that the request is for several hundred
-                            // datapoints each consisting of a few steps and a timestamp.  The more likely
-                            // scenario is wanting to see how many steps were walked per day, for 7 days.
                             .read(DataType.TYPE_STEP_COUNT_DELTA)
-                            // Analogous to a "Group By" in SQL, defines how data should be aggregated.
-                            // bucketByTime allows for a time span, whereas bucketBySession would allow
-                            // bucketing by "sessions", which would need to be defined in code.
                             .bucketByTime(1, TimeUnit.DAYS)
                             .setTimeRange(startTime, endTime, TimeUnit.MILLISECONDS)
                             .enableServerQueries()
                             .build();
             Task<DataReadResponse> response = Fitness.getHistoryClient(getApplicationContext(), GoogleSignIn.getLastSignedInAccount(getApplicationContext())).readData(readRequest);
-//            DataReadResponse result = response.getResult();
             DataReadResponse result = null;
             try {
                 result = Tasks.await(response);
--- app/src/main/java/com/aki/stepstracker/utils/StepsDataParser.java
+++ app/src/main/java/com/aki/stepstracker/utils/StepsDataParser.java
@@ -12,41 +12,27 @@ import java.text.DateFormat;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
-import static java.text.DateFormat.getDateTimeInstance;
-
 public class StepsDataParser {
     private static final String TAG = "data parser";
-
-    public static StepInfo dumpDataSet(DataSet dataSet) {
-        Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
-        DateFormat dateFormat = getDateTimeInstance();
-        int totalSteps = 0;
-//        String date = dateFormat.format(dataSet.getDataPoints().get(0).getStartTime(TimeUnit.MILLISECONDS));
-        for (DataPoint dp : dataSet.getDataPoints()) {
-//            Log.i(TAG, "Data point:");
-//            Log.i(TAG, "\tType: " + dp.getDataType().getName());
-            Log.i(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
-            Log.i(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
-            for (Field field : dp.getDataType().getFields()) {
-                Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
-                totalSteps = totalSteps + dp.getValue(field).asInt();
-            }
-        }
-        Log.i(TAG, "\tSteps for day: " + totalSteps);
-        return new StepInfo(DateFormat.getDateInstance().format(dataSet.getDataPoints().get(0).getStartTime(TimeUnit.MILLISECONDS)), totalSteps);
-    }
 
     public static StepInfo getDataFromBucket(Bucket bucket) {
         DateFormat dateFormat = DateFormat.getDateInstance();
         List<DataSet> dataSets = bucket.getDataSets();
         int totalSteps = 0;
         for (DataSet dataSet : dataSets) {
+            Log.i(TAG, "Data returned for Data type: " + dataSet.getDataType().getName());
             for (DataPoint dp : dataSet.getDataPoints()) {
-                for(Field field : dp.getDataType().getFields()) {
+                Log.i(TAG, "Data point:");
+                Log.i(TAG, "\tType: " + dp.getDataType().getName());
+                Log.i(TAG, "\tStart: " + dateFormat.format(dp.getStartTime(TimeUnit.MILLISECONDS)));
+                Log.i(TAG, "\tEnd: " + dateFormat.format(dp.getEndTime(TimeUnit.MILLISECONDS)));
+                for (Field field : dp.getDataType().getFields()) {
+                    Log.i(TAG, "\tField: " + field.getName() + " Value: " + dp.getValue(field));
                     totalSteps = totalSteps + dp.getValue(field).asInt();
                 }
             }
+            Log.i(TAG, "\tSteps for day: " + totalSteps);
         }
-        return new StepInfo(dateFormat.format(bucket.getStartTime(TimeUnit.MILLISECONDS)),totalSteps);
+        return new StepInfo(dateFormat.format(bucket.getStartTime(TimeUnit.MILLISECONDS)), totalSteps);
     }
 }
